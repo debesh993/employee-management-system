29f1b7eb7688f6a56fcb1c06669f32890106e023
Debesh Dinda <debeshdinda993@gmail.com>
2026-02-10 16:08:44 +0530

    Changing cors

--- server/index.js
+++ server/index.js
@@ -14,10 +14,19 @@ dotenv.config();
 connecttodatabase()
 
 const app=express()
+// app.use(cors({
+//     origin:"https://employee-management-system-frontend-sand.vercel.app",
+//     credentials:true
+// }))
 app.use(cors({
-    origin:"https://employee-management-system-frontend-sand.vercel.app",
-    credentials:true
+  origin: "https://employee-management-system-frontend-sand.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
+
+app.options("*", cors())
+
 app.use(express.json())
 app.use(express.static('public/uploads'))
 app.use('/api/auth',authRouter)
